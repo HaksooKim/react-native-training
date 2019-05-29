6b0e6a83935d8f10e12bf162f069fc5c8005a053
Haksoo Kim <hsouhy@gmail.com>
2019-05-29 23:37:25 +0900

    add1

---
+++ src/includes.js
@@ -0,0 +1,5 @@
+let years = [2001, 2010, 2015, 2018];
+
+console.log(years.indexOf(2018) !== -1);
+
+console.log(years.includes(2018));
---
+++ src/restOperator.js
@@ -0,0 +1,11 @@
+function printYears(years) {
+    console.log(years);
+}
+
+printYears(2001, 2010, 2015, 2018);
+
+function printYears2(...years) {
+    console.log(years);
+}
+
+printYears2(2001, 2010, 2015, 2018);
